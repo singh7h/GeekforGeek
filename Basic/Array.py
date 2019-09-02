ce925af0da3cbda602bfd8f1b714ca6d0edd92f5
Harman Singh <boparai74@gmail.com>
2019-09-02 17:16:39 -0500

    Python Array

---
+++ Basic/Array.py
@@ -0,0 +1,27 @@
+
+"""Sum of array"""
+def _sum(arr):
+    return sum(arr)
+
+def splitarr(arr, d):
+    for i in range(d):
+        new_arr = arr
+        x = arr.pop(0)
+        new_arr.append(x)
+        print(new_arr)
+
+def largest(arr,d):
+     large = arr[0]
+     for i in range(0, d):
+         if arr[i] > large:
+             large = arr[i]
+     print(large)
+
+
+
+if __name__ == '__main__':
+    arr = [6, 2, 3, 4, 5,90,88]
+    #splitarr(arr, 2)
+    n = len(arr)
+    largest(arr, n)
+
